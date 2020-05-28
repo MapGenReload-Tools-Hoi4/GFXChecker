ccb34da2f4464953b40861da9623cfc874a78c36
Sepera_okeq <sepera173@gmail.com>
2020-05-29 02:42:05 +0400

    up

--- GFXChecker/startwindow.h
+++ GFXChecker/startwindow.h
@@ -81,6 +81,33 @@ public:
         return line;
     }
 
+    QString listfilesindir(QString path)
+    {
+        /*
+        Функция считывает список файлов (включая скрытые) в директории в QString.
+        Входные параметры:
+         path - путь к папке.
+        Возвращаемое значение:
+         Строка со списком файлов в директории, разделенные n в алфавитном порядке.
+        */
+        QString line="";
+        QDir dir(path);
+        bool ok = dir.exists();
+        if (ok)
+        {
+            dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+            dir.setSorting(QDir::Name);
+            QFileInfoList list = dir.entryInfoList();
+
+            for (int i = 0; i < list.size(); ++i)
+            {
+                QFileInfo fileInfo = list.at(i);
+                line+=fileInfo.fileName()+"\n";
+            }
+        }
+        return line;
+    }
+
 
     void saveJson(QJsonDocument settings,QString fileName) {
         QFile jsonFile(fileName);
